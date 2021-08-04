d930c9e604143a1d7a053ab76028da9de992b8b0
Harpreet Singh <singhh.harpreet19@gmail.com>
2021-08-04 17:38:29 +0530

    locking site

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="app">
-    <reader></reader>
+    <b-modal
+      v-if="!viewContent"
+      ref="verify"
+      @ok="submit"
+      :ok-disabled="popupOk"
+      ok-only
+      @hidden="closePopup"
+      title="Give password"
+      :no-close-on-backdrop="true"
+      :no-close-on-esc="true"
+      data-backdrop="static"
+      centered
+    >
+      <label for="p">Password</label>&nbsp;
+      <input type="password" v-model="pass"  @mouseleave="validate"/>
+    </b-modal>
+    <reader v-if="viewContent"></reader>
   </div>
 </template>
 
@@ -9,6 +25,34 @@ import reader from "@/components/reader.vue";
 export default {
   name: "App",
   components: { reader },
+  data() {
+    return {
+      pass: "",
+      viewContent: false,
+      popupOk: true,
+    };
+  },
+  methods: {
+    validate() {
+      if (this.pass == "admin") {
+        this.popupOk = false;
+      }
+      else{
+        this.popupOk=true
+      }
+    },
+    submit() {
+      if (this.pass == "admin") {
+        this.viewContent = true;
+      }
+    },
+    closePopup() {
+      this.$refs.verify.hide();
+    },
+  },
+  mounted() {
+    this.$refs.verify.show();
+  },
 };
 </script>
 
@@ -21,4 +65,5 @@ export default {
   margin: 0;
   padding: 0;
 }
+.close {display: none;}
 </style>
